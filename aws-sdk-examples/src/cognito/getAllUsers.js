b4d3bbf3d67c994fc5433fe366393de158175249
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-01-01 01:05:01 +0500

    cognito example added in aws-sdk-examples

---
+++ aws-sdk-examples/src/cognito/getAllUsers.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+require('dotenv').config({ path: '../.env' });
+const REGION = process.env.REGION;
+const awsConfig = {
+    region: REGION,
+    // "endpoint": `http://cognito.${REGION}.amazonaws.com`,
+    "accessKeyId": process.env.ACCESS_KEY_ID,
+    'secretAccessKey': process.env.SECRET_ACCESS_KEY,
+};
+AWS.config.update({ ...awsConfig });
+AWS.config.region = REGION;
+
+const cognitoServiceProvider = new AWS.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18', region: REGION });
+
+//////////////////////////////////////////////////////////////////////
+// ref  https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CognitoIdentityServiceProvider.html#listUsers-property
+
+var params = {
+    UserPoolId: process.env.USER_POOL_ID, /* required */
+    // AttributesToGet: [
+    //   'STRING_VALUE',
+    //   /* more items */
+    // ],
+    // Filter: 'STRING_VALUE',
+    // Limit: 'NUMBER_VALUE',
+    // PaginationToken: 'STRING_VALUE'
+};
+cognitoServiceProvider.listUsers(params, function (err, data) {
+    if (err) console.log("ERROR==>", err, "\nERROR.STACK==>", err.stack); // an error occurred
+    else console.log(JSON.stringify(data, null, 2));// successful response
+});
